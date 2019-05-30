7f927ca48a75e1703029bed63d89884294a6a3db
JimmyAnthony <xim.jabs@gmail.com>
2019-05-29 23:53:27 -0500

    Change

--- apps/modules/mobil/models/indexModels.php
+++ apps/modules/mobil/models/indexModels.php
@@ -50,7 +50,7 @@ class indexModels extends Adodb {
         parent::SetParameterSP(sha1($p['password']), 'varchar');
         parent::SetParameterSP($p['mac'], 'varchar');
         parent::SetParameterSP($p['numero'], 'varchar');
-        // echo '=>' . parent::getSql().'<br>'; exit();
+         echo '=>' . parent::getSql().'<br>'; exit();
         $array = parent::ExecuteSPArray();
         return $array;
     }
